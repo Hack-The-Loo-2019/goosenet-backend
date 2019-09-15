9347f07fbdd21a2cae31371ffa1563361b609250
Ruben Partono <ruben.partono@setter.com>
2019-09-14 22:22:40 -0400

    Implement creating a fake client name and ignore DS_Store

---
+++ functions/controllers/users/utils/fakeClient.js
@@ -0,0 +1,28 @@
+const faker = require('faker')
+const animals = require('animals')
+
+const adjectiveGenerators = [
+    faker.hacker.adjective,
+    faker.commerce.productAdjective,
+    faker.company.bsAdjective,
+    faker.company.catchPhraseAdjective,
+]
+
+const randomAdjective = () => {
+    const randomIndex = faker.random.number({
+        min: 0,
+        max: adjectiveGenerators.length - 1
+    })
+    return adjectiveGenerators[randomIndex]()
+}
+
+const fakeClient = () => {
+    const adjective = randomAdjective()
+    const animal = animals()
+
+    const name = `${adjective} ${animal}`
+  
+    return { name }
+}
+
+module.exports = fakeClient
