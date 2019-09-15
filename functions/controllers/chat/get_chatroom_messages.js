7e5f5c1fd81cae146329558027ad9456ec1bd346
Ruben Partono <ruben.partono@setter.com>
2019-09-15 00:51:36 -0400

    Make get chatroom messages work

--- functions/controllers/chat/get_chatroom_messages.js
+++ functions/controllers/chat/get_chatroom_messages.js
@@ -1,6 +1,7 @@
 const db = require('firebase-admin').firestore();
 const build_error = require('../utils/build_error');
 const get_user_data = require('../utils/get_user_data');
+const areFriends = require('../utils/are_friends')
 
 module.exports = async (req, res, next) => {
     const { chatroom_id } = req.params;
@@ -8,10 +9,33 @@ module.exports = async (req, res, next) => {
 
     const chatroomRef = db.collection('Chatrooms').doc(chatroom_id);
     const messageRefs = chatroomRef.collection('Messages').orderBy('timestamp', 'desc');
-    console.log('hey')
     const messageSnaps = (await messageRefs.get()).docs
-    console.log('huhh')
-    const messageDatas = messageSnaps.map(snapshot => snapshot.data());
-    console.log('henloo')
-    res.status(200).send({ data: messageDatas });
+    const messageDataPromises = messageSnaps.map(async snapshot => {
+        
+        const data = {}
+        data.id = snapshot.id
+        const rawData = snapshot.data()
+        const showFake = !areFriends(req, rawData.user_ref.id)
+
+        if (showFake) {
+            const fakeSnapshot = await rawData.fake_ref.get()
+            const fakeData = fakeSnapshot.data()
+            data.user = {
+                id: fakeSnapshot.id,
+                name: fakeData.name
+            }
+        } else {
+            const userSnapshot = await rawData.user_ref.get()
+            const userData = userSnapshot.data()
+            data.user = {
+                id: rawData.user_ref.id, 
+                name: userData.name
+            }
+        }
+        data.message = rawData.message
+        data.timestamp = new Date(rawData.timestamp._seconds * 1000)
+        return data
+    });
+    const messageDatas = await Promise.all(messageDataPromises)
+    res.status(200).send({ data: messageDatas })
 };
