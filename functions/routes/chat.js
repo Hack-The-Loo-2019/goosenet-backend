a63239f993aa7750b90475ca757d16a339c8d56c
timweri <ng.duc.tahn@gmail.com>
2019-09-15 04:43:37 -0400

    Disable chatroom permission check
    Signed-off-by: timweri <ng.duc.tahn@gmail.com>

--- functions/routes/chat.js
+++ functions/routes/chat.js
@@ -4,7 +4,7 @@ const chat_controllers = require('../controllers/chat');
 const auth_controllers = require('../controllers/users/authenticate');
 
 router.use(auth_controllers);
-router.use('/:chatroom_id', chat_controllers.checkChatroomPermission);
+// router.use('/:chatroom_id', chat_controllers.checkChatroomPermission);
 router.put('/:chatroom_id', chat_controllers.addChatroomMessage);
 router.get('/:chatroom_id', chat_controllers.getChatroomMessages);
 
