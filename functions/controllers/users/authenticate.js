61fde3ae7b6022a90c1403b52239f0d2ed595cb8
Ruben Partono <ruben.partono@setter.com>
2019-09-14 21:04:04 -0400

    Add fake authentication middleware

---
+++ functions/controllers/users/authenticate.js
@@ -0,0 +1,26 @@
+const firebase_admin = require('firebase-admin')
+
+const db = firebase_admin.firestore();
+
+// For now, header only needs to be 
+// { x-auth: ${userId} }
+// Can upgrade later on
+module.exports = async (req, res, next) => {
+    const authHeader = req.header('x-auth');
+    if (!authHeader) {
+        res.status(401).json({ error: `Missing auth header` })
+    }
+    console.log(authHeader)
+    const doc = await db
+        .collection('Users')
+        .doc(authHeader)
+        .get()
+    if (!!doc.exists) {
+        req.userId = authHeader
+        next()
+        console.log('reached here')
+    }
+    else {
+        res.status(401).json({ error: `User ${authHeader} does not exist` })
+    }
+}
--- functions/routes/users.js
+++ functions/routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const users_controllers = require('../controllers/users');
+const auth_controllers = require('../controllers/users/authenticate')
 
+router.use(auth_controllers)
 router.get('/:id', users_controllers.getById);
 
 module.exports = router;
